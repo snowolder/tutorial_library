822f6ad0dcbb897d15cf75aef2319192ba9a64af
DevDrache <dominik.mast.11@gmail.com>
2026-08-29 15:40:34 +0300

    Add files via upload

---
+++ UI/simple_ui_hover_animation/level_container.gd
@@ -0,0 +1,41 @@
+extends PanelContainer
+
+@export var styleBoxSelected: StyleBox
+@export var styleBoxUnselected: StyleBox
+
+@onready var level_label: Label = %levelLabel
+@onready var icon: TextureRect = $MarginContainer/VBoxContainer/icon
+
+const CHECK = preload("uid://dbnvm4tb1ss3y")
+const PADLOCK = preload("uid://cganrn6vdthrl")
+
+var levelNumber: int = -1:
+	set(value):
+		levelNumber = value
+		%levelLabel.text = str(value)
+
+func set_lock():
+	icon.modulate.a = 1
+	icon.texture = PADLOCK
+	
+func set_open():
+	icon.modulate.a = 0
+
+func set_done():
+	icon.modulate.a = 1
+	icon.texture = CHECK
+
+
+func _on_mouse_entered() -> void:
+	add_theme_stylebox_override("panel", styleBoxSelected)
+	var tween := create_tween()
+	tween.tween_property(self, "offset_transform_rotation",deg_to_rad(10), 0.1)
+	tween.parallel()
+	tween.tween_property(self, "offset_transform_position",Vector2(0,-10), 0.1)
+	
+func _on_mouse_exited() -> void:
+	add_theme_stylebox_override("panel", styleBoxUnselected)
+	var tween := create_tween()
+	tween.tween_property(self, "offset_transform_rotation",deg_to_rad(0), 0.1)
+	tween.parallel()
+	tween.tween_property(self, "offset_transform_position",Vector2(0,0), 0.1)
